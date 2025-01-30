d7a6f5b030acb1238f7ce9abbe74e4fe1248d220
nicorasck <nicholas.rasck@gmail.com>
2025-01-30 21:45:32 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+
+using BlazorApiEf_Task.Data;
+using Microsoft.EntityFrameworkCore;
+using BlazorApiEf.Data;
+
+namespace BlazorApiEf_Task;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+        builder.Services.AddSingleton<WeatherForecastService>();
+
+
+        builder.Services.AddDbContext<UserDbContext>(options =>
+        options.UseSqlite("Data Source=BlazorApiEf_Task.db"));
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.MapBlazorHub();
+        app.MapFallbackToPage("/_Host");
+
+        app.Run();
+    }
+}
